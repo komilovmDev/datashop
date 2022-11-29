e302ef2c58ce2361d3f6b3c114bc14074ea1028f
AkbarAli <satipovakbar@gmail.com>
2022-11-29 17:13:41 +0500

    sett

--- datashop/settings.py
+++ datashop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["datashop.uz", "localhost"]
 
 # CART
 
