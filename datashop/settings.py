fd0355e462ddfd04902b85f22ba6acbba3d081b3
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-12-06 23:01:02 +0500

    www setingss

--- datashop/settings.py
+++ datashop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["datashop.uz", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["datashop.uz", "www.datashop.uz", ".datashop.uz", "localhost", "127.0.0.1"]
 
 # CART
 
