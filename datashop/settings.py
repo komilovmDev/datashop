216b016ce53c4810db70b84524f784e20e83ea3a
AkbarAli <satipovakbar@gmail.com>
2022-12-14 12:11:27 +0500

    staticroot file

--- datashop/settings.py
+++ datashop/settings.py
@@ -150,7 +150,7 @@ USE_THOUSAND_SEPARATOR=True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
