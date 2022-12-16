5ac584bc9e2e676e04807555232bc6f9bcc9a3d4
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-12-16 10:11:10 +0500

    Admin Panel Fix

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -8,10 +8,16 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 4
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'price']
     prepopulated_fields = {'slug': ('title',)}
+    inlines = [ProductImageInline]
 
 
 @admin.register(ProductImage)
